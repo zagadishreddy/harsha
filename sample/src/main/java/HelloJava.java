c54276f5b04823cc842d5b1a2597c151c687e363
“JAGADISH <bachujagadeswara.Reddy@ge.com>
2016-02-24 20:48:51 +0530

    sample commmit

---
+++ sample/src/main/java/HelloJava.java
@@ -0,0 +1,13 @@
+
+public class HelloJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world..!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ sample/src/main/java/com/ge/HelloJava.java
@@ -0,0 +1,14 @@
+package com.ge;
+
+public class HelloJava {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world..!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
